fce9cd1c599f3d8ca247520f28d6bc8389ee6844
eberwild <kevinflotow@arcor.de>
2026-03-20 17:08:16 +0100

    design detailsPage first look

--- app/(lists)/menu/[id].jsx
+++ app/(lists)/menu/[id].jsx
@@ -1,11 +1,13 @@
 import MenuImages from "@/constants/MenuImages";
 import { MENU_ITEMS } from "@/constants/MenuItems";
 import { useLocalSearchParams } from "expo-router";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Detail() {
   const { id } = useLocalSearchParams();
+  const [value, setValue] = useState(0);
 
   const { title, description } = MENU_ITEMS.find(
     (item) => item.id.toString() === id,
@@ -22,6 +24,27 @@ export default function Detail() {
 
         <View style={styles.textContainer}>
           <Text style={styles.text}>{description}</Text>
+        </View>
+      </View>
+
+      <View style={styles.orderContainer}>
+        <Text style={styles.orderText}>Order {title} here:</Text>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="-"
+            style={styles.button}
+            onPress={() => {
+              setValue((prev) => prev - 1);
+            }}
+          ></Button>
+          <Text>{value}</Text>
+          <Button
+            title="+"
+            style={styles.button}
+            onPress={() => {
+              setValue((prev) => prev + 1);
+            }}
+          ></Button>
         </View>
       </View>
     </SafeAreaView>
@@ -59,5 +82,28 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     marginBottom: 30,
     marginTop: 30,
+  },
+  orderContainer: {
+    backgroundColor: "rgba(104, 98, 98, 0.8)",
+    flexDirection: "column",
+    marginTop: 50,
+    width: "95%",
+    borderRadius: 10,
+  },
+  buttonContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+    padding: 10,
+  },
+  orderText: {
+    color: "white",
+    marginBottom: 25,
+    alignSelf: "center",
+  },
+  button: {
+    color: "rgba(218, 130, 15, 0.6)",
+    borderRadius: 10,
   },
 });
