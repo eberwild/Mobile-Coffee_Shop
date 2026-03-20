a7f1bd6dfb03a9256a8c4467ca03a9690800f017
Kevin <kevinflotow@arcor.de>
2026-03-20 15:45:01 +0100

    code indentation

--- app/(lists)/menu/[id].jsx
+++ app/(lists)/menu/[id].jsx
@@ -1,65 +1,63 @@
-import { View, Text , StyleSheet , Image} from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { useLocalSearchParams } from 'expo-router';
-import { MENU_ITEMS } from '@/constants/MenuItems';
-import MenuImages from '@/constants/MenuImages';
+import MenuImages from "@/constants/MenuImages";
+import { MENU_ITEMS } from "@/constants/MenuItems";
+import { useLocalSearchParams } from "expo-router";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Detail() {
+  const { id } = useLocalSearchParams();
 
-    const { id } = useLocalSearchParams();
+  const { title, description } = MENU_ITEMS.find(
+    (item) => item.id.toString() === id,
+  );
 
-    const { title , description} = MENU_ITEMS.find(item => item.id.toString() === id);
+  const image = MenuImages[id - 1];
 
-    const image = MenuImages[id -1];
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Text style={styles.title}>{title}</Text>
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <View>
-                <Text style={styles.title}>{title}</Text>
+        <Image source={image} style={styles.image} />
 
-                <Image source={image}
-                        style={styles.image}
-                />
-                
-                <View style={styles.textContainer}>
-                    <Text style={styles.text}>{description}</Text>
-                </View>
-            
-            </View>
-        </SafeAreaView>
-    );
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>{description}</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        justifyContent: 'start',
-        alignItems: 'center',
-        marginTop: 10,
-    },
-    title: {
-        fontSize: 30,
-        alignSelf: 'center',
-        color: 'rgba(218, 130, 15, 0.6)'
-    },
-    text: {
-        color: 'white',
-        fontSize: 19,
-        textAlign: 'center'
-    },
-    textContainer: {
-        backgroundColor: 'rgba(218, 130, 15, 0.6)',
-        padding: 15,
-        marginHorizontal: 10,
-        borderRadius: 10
-    },
-    image: {
-        resizeMode: 'cover',
-        borderRadius: 15,
-        alignSelf: 'center',
-        borderColor: 'rgba(218, 130, 15, 0.6)',
-        borderWidth: 1,
-        marginBottom: 30,
-        marginTop: 30
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "start",
+    alignItems: "center",
+    marginTop: 10,
+  },
+  title: {
+    fontSize: 30,
+    alignSelf: "center",
+    color: "rgba(218, 130, 15, 0.6)",
+  },
+  text: {
+    color: "white",
+    fontSize: 19,
+    textAlign: "center",
+  },
+  textContainer: {
+    backgroundColor: "rgba(218, 130, 15, 0.6)",
+    padding: 15,
+    marginHorizontal: 10,
+    borderRadius: 10,
+  },
+  image: {
+    resizeMode: "cover",
+    borderRadius: 15,
+    alignSelf: "center",
+    borderColor: "rgba(218, 130, 15, 0.6)",
+    borderWidth: 1,
+    marginBottom: 30,
+    marginTop: 30,
+  },
+});
